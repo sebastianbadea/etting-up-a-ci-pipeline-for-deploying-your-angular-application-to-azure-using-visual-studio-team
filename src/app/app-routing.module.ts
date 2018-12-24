172f8164c86fb8f87fcb5f66a5ecd140b7de3b4e
Sebastian Badea <sebastian.badea@gmail.com>
2018-12-24 14:58:15 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'red',
+        component: RedComponent
+      },
+      {
+        path: 'blue',
+        component: BlueComponent
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedComponent,
+    BlueComponent
   ],
   imports: [
     BrowserModule,
